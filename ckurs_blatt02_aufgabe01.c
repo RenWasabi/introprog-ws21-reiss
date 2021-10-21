e05ab42faa4f7de593f7da92830aa84c3b3c40ea
Carolina Reiß <reiss@campus.tu-berlin.de>
2021-10-21 08:29:50 +0200

    Vorgabedatei Kurs hinzugefügt

---
+++ ckurs_blatt02_aufgabe01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int nummer = 105; // Probiere verschiedene Werte aus: 101, 103, ...
+    printf("Ist %d eine Primzahl?\n", nummer);
+
+    // HIER Code einfügen
+
+    return 0;
+}
