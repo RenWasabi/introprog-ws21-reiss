0b10175f3a9295d94a7815d71c33420ab77f4dba
Carolina Reiß <reiss@campus.tu-berlin.de>
2021-10-23 17:59:11 +0200

    Aufgabe 3, hoehe breite 0 eingesetzt

--- ckurs_blatt03_aufgabe01.c
+++ ckurs_blatt03_aufgabe01.c
@@ -23,7 +23,7 @@ int main() {
 	for (int i=0; i <= breite-1; i++) { // gibt A in jeder Spalte aus
 	  printf("A ");
 	}
-	//printf("\n"); // keine Leerzeile nach unterster Zeile ist eigentlich Aufgabenvorgabe
+	printf("\n"); // keine Leerzeile nach unterster Zeile ist eigentlich Aufgabenvorgabe
       } // Ende else-if-Schleife Rand unten
 
       else { // falls weder oberste noch unterste, somit mittlere Zeile
@@ -43,4 +43,47 @@ int main() {
     } // Ende for-Schleife Zeilen
 
 } // Ende else-Schleife, falls hoehe/breite nicht zulässig
+
+         breite = 0;
+     hoehe = 0;
+
+    if (breite < 0) {    // macht nichts, falls hoehe oder breite keine positiven Zahlen
+      printf("\n");
+    } else if (hoehe < 0) {
+      printf("\n");
+    } else {
+
+    for (int j=0; j <= hoehe-1; j++) {  // geht zuerst jede Zeile, dann jede Spalte durch
+      
+      if (j==0) {  // wenn oberste Zeile:
+	for (int i=0; i <= breite-1; i++) { // gibt A in jeder Spalte aus
+	  printf("A ");
+	}
+	printf("\n"); // setze Umbruch hinter oberste Zeile       
+      } // Ende if-Schleife Rand oben
+
+      else if (j==hoehe-1) {  // wenn unterste Zeile:
+	for (int i=0; i <= breite-1; i++) { // gibt A in jeder Spalte aus
+	  printf("A ");
+	}
+	printf("\n"); // keine Leerzeile nach unterster Zeile ist eigentlich Aufgabenvorgabe
+      } // Ende else-if-Schleife Rand unten
+
+      else { // falls weder oberste noch unterste, somit mittlere Zeile
+	for (int i=0; i<= breite-1; i++) { // für jede mittlere Zeile
+	  if (i==0) { // am linken Rand: gibt A aus
+	    printf("A "); 
+	  }
+	  else if (i==breite-1) { // am rechten Rand: gibt A aus
+	    printf("A ");
+	  }
+	  else { // in den mittleren Spalten: gibt B aus
+	    printf("B ");
+	  }
+	} // Ende for-Schleife Spalten
+        printf("\n");  // setze Umbruch hinter jede mittlere Zeile	
+      } // Ende else-Schleife Mitte
+    } // Ende for-Schleife Zeilen
+
+} // Ende else-Schleife, falls hoehe/breite nicht zulässig   
 }  // Eine main-Fkt
