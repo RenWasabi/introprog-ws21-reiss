3889e10c0f174642b39d919534c5225977a26e7e
Carolina Reiß <reiss@campus.tu-berlin.de>
2021-10-23 17:30:59 +0200

    Aufgabe 3 funktioniert

--- ckurs_blatt03_aufgabe01.c
+++ ckurs_blatt03_aufgabe01.c
@@ -12,6 +12,28 @@ int main() {
 	}
 	printf("\n"); // setze Umbruch hinter oberste Zeile       
       } // Ende if-Schleife Rand oben
+
+      else if (j==hoehe-1) {  // wenn unterste Zeile:
+	for (int i=0; i < breite; i++) { // gibt A in jeder Spalte aus
+	  printf("A ");
+	}
+	printf("\n"); // keine Leerzeile nach unterster Zeile ist eigentlich Aufgabenvorgabe
+      } // Ende else-if-Schleife Rand unten
+
+      else { // falls weder oberste noch unterste, somit mittlere Zeile
+	for (int i=0; i<breite; i++) { // für jede mittlere Zeile
+	  if (i==0) { // am linken Rand: gibt A aus
+	    printf("A "); 
+	  }
+	  else if (i==breite-1) { // am rechten Rand: gibt A aus
+	    printf("A ");
+	  }
+	  else { // in den mittleren Spalten: gibt B aus
+	    printf("B ");
+	  }
+	} // Ende for-Schleife Spalten
+        printf("\n");  // setze Umbruch hinter jede mittlere Zeile	
+      } // Ende else-Schleife Mitte
       
     } // Ende for-Schleife Zeilen
 }  // Eine main-Fkt
