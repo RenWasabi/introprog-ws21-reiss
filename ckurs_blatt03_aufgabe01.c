42768012be9242ec32e359c6e1983580c2a03a37
Carolina Reiß <reiss@campus.tu-berlin.de>
2021-10-23 17:11:15 +0200

    Aufgabe 3 Ausgabe erster Zeile funktioniert

--- ckurs_blatt03_aufgabe01.c
+++ ckurs_blatt03_aufgabe01.c
@@ -4,6 +4,14 @@ int main() {
     int breite = 6;
     int hoehe = 3;
 
-    // Hier Code einfügen
-
-}
+    for (int j=0; j < hoehe; j++) {  // geht zuerst jede Zeile, dann jede Spalte durch
+      
+      if (j==0) {  // wenn oberste Zeile:
+	for (int i=0; i < breite; i++) { // gibt A in jeder Spalte aus
+	  printf("A ");
+	}
+	printf("\n"); // setze Umbruch hinter oberste Zeile       
+      } // Ende if-Schleife Rand oben
+      
+    } // Ende for-Schleife Zeilen
+}  // Eine main-Fkt
