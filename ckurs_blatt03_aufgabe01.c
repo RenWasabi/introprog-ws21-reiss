61ed5a119de3285e18d4165b5b32860d155b6a01
Carolina Reiß <reiss@campus.tu-berlin.de>
2021-10-23 17:44:41 +0200

    Aufgabe 3, Versuch, hoehe oder breite = 0 zuzulassen

--- ckurs_blatt03_aufgabe01.c
+++ ckurs_blatt03_aufgabe01.c
@@ -4,30 +4,30 @@ int main() {
     int breite = 6;
     int hoehe = 3;
 
-    if (breite <= 0) {    // macht nichts, falls hoehe oder breite keine positiven Zahlen
+    if (breite < 0) {    // macht nichts, falls hoehe oder breite keine positiven Zahlen
       return 0;
-    } else if (hoehe <= 0) {
+    } else if (hoehe < 0) {
     return 0;
     } else {
 
-    for (int j=0; j < hoehe; j++) {  // geht zuerst jede Zeile, dann jede Spalte durch
+    for (int j=0; j <= hoehe-1; j++) {  // geht zuerst jede Zeile, dann jede Spalte durch
       
       if (j==0) {  // wenn oberste Zeile:
-	for (int i=0; i < breite; i++) { // gibt A in jeder Spalte aus
+	for (int i=0; i <= breite-1; i++) { // gibt A in jeder Spalte aus
 	  printf("A ");
 	}
 	printf("\n"); // setze Umbruch hinter oberste Zeile       
       } // Ende if-Schleife Rand oben
 
       else if (j==hoehe-1) {  // wenn unterste Zeile:
-	for (int i=0; i < breite; i++) { // gibt A in jeder Spalte aus
+	for (int i=0; i <= breite-1; i++) { // gibt A in jeder Spalte aus
 	  printf("A ");
 	}
 	printf("\n"); // keine Leerzeile nach unterster Zeile ist eigentlich Aufgabenvorgabe
       } // Ende else-if-Schleife Rand unten
 
       else { // falls weder oberste noch unterste, somit mittlere Zeile
-	for (int i=0; i<breite; i++) { // für jede mittlere Zeile
+	for (int i=0; i<= breite-1; i++) { // für jede mittlere Zeile
 	  if (i==0) { // am linken Rand: gibt A aus
 	    printf("A "); 
 	  }
