07cba81978aa8acb605a399c017f75394abbe05d
Carolina Reiß <reiss@campus.tu-berlin.de>
2021-10-26 01:17:38 +0200

    Fibonacci funktioniert

--- ckurs_blatt04_aufgabe01.c
+++ ckurs_blatt04_aufgabe01.c
@@ -3,6 +3,19 @@
 #include "input.h" // Hier binden wir die Bibliothek ein
 
 // Schreibe hier die Funktion "int fibonacci"
+int fibonacci(zahl) {  // zahl erhält fib durch lese_int
+  if (zahl==1) {       // Anker 1: f(1)=1 (tritt nur für n=1 ein)
+    //    printf("fib(%d)= 1\n",zahl);
+    return 1;
+  }
+  else if (zahl==2) {  // Anker 2: f(2)=1
+    // printf("fib(%d)= 1\n",zahl);    
+    return 1; 
+  }
+  int result = fibonacci(zahl-1)+fibonacci(zahl-2); // falls kein Anker, rufe Rekursion
+  //  printf("fib(%d)= %d\n", zahl, result);
+  return result;
+}
 
 int main(){
     int n = lese_int(); // Hier rufen wir die Funktion lese_int auf.
