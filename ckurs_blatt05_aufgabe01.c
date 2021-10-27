ad1a9aacd73587ec7ca6778c79eeccf50c439814
Carolina Reiß <reiss@campus.tu-berlin.de>
2021-10-27 18:59:44 +0200

    Vorgabedatei hinzugefügt

---
+++ ckurs_blatt05_aufgabe01.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_array(int array[], int len) {
+    // HIER Code einfügen
+}
+
+// Schreibe die Funktion "sum", "min" und "max"
+
+int main() {
+    int array[] = {9, 4, 7, 8, 10, 5, 1, 6, 3, 2};
+    int len = 10;
+    print_array(array, len);
+    // Gebe hier nacheinander das Minimum, Maximum und die Summe
+    // aus. Trenne die Werte durch einen einzelnen Zeilenumbruch.
+}
