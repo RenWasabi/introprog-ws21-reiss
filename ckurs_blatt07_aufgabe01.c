34986fd8f0b686e681663c55fb6997468e39991e
Carolina Reiß <reiss@campus.tu-berlin.de>
2021-10-30 09:01:37 +0200

    Vorgabedatei Blatt 7 Aufgabe 1

---
+++ ckurs_blatt07_aufgabe01.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    char wochentag[] = "Freitag";
+    int tag = 13;
+    char monat[] = "Mai";
+    int jahr = 1927;
+    char *string; // Hier soll das Datum hineingeschrieben werden!
+    const size_t kapazitaet; // Maximale Laenge des Datumstrings!
+
+    // Hier implementieren und dynamisch Speicher reservieren
+
+    printf("%s\n", string);
+    // Speicher freigeben
+
+    return 0;
+}
