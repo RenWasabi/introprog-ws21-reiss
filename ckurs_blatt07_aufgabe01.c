ba1e96010e19290d268ef5bbdb360e413316d1b9
Carolina Reiß <reiss@campus.tu-berlin.de>
2021-10-30 09:10:25 +0200

    Blatt 07 Aufgabe 1 funktioniert

--- ckurs_blatt07_aufgabe01.c
+++ ckurs_blatt07_aufgabe01.c
@@ -8,12 +8,24 @@ int main() {
     char monat[] = "Mai";
     int jahr = 1927;
     char *string; // Hier soll das Datum hineingeschrieben werden!
-    const size_t kapazitaet; // Maximale Laenge des Datumstrings!
+    const size_t kapazitaet = 101 * sizeof(char); // Maximale Laenge des Datumstrings!
 
     // Hier implementieren und dynamisch Speicher reservieren
 
+    // es darf angenommen werden: maximale Datumslänge sind 100 Zeichen
+    // snprintf schließt string mit Nullbyte ab, also Länge +1
+    /*    kapazitaet = 101 * sizeof(char); das hier gibt einen Fehler beim Compilen,
+     * vielleicht kann die Konstante nur bei ihrer Deklaration festlegen? */
+
+    // Speicher dieser Größe da, wo pointer string hinzeigt, anfordern
+    string = (char*) malloc(kapazitaet);
+
+    // das Datum in den String drucken
+    snprintf(string, kapazitaet, "%s, der %d. %s %d\n", wochentag, tag, monat, jahr);
+
     printf("%s\n", string);
     // Speicher freigeben
+    free(string);
 
     return 0;
 }
