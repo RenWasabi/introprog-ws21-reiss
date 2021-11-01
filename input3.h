c7565eeb3f0bea6208fca4affc26477a7ee156d5
Carolina Reiß <reiss@campus.tu-berlin.de>
2021-11-01 19:01:02 +0100

    Aufgabe2 neu, Bundesland-Filter funktioniert

---
+++ input3.h
@@ -0,0 +1,5 @@
+extern int MAX_LAENGE_ARR;
+extern int MAX_LAENGE_STR;
+
+void write_file(char *result[], int len);
+int read_file(char *dateiname, char staedte[][MAX_LAENGE_STR], char laender[][MAX_LAENGE_STR], int bewohner []);
