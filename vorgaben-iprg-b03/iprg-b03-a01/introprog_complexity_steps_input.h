316154e573104258772b284a55b2ebcea8e496ea
Carolina <reiss@campus.tu-berlin.de>
2021-11-20 14:21:19 +0100

    benötige Input-Datei hinzugefügt

---
+++ vorgaben-iprg-b03/iprg-b03-a01/introprog_complexity_steps_input.h
@@ -0,0 +1,9 @@
+void fill_array_randomly(int array[], int len, int max_value);
+void fill_array_deterministicly(int array[], int len, int max_value);
+void print_array(int array[], int len);
+
+int check_equality_of_arrays(int* array_1, int* array_2, int len);
+void copy_array_elements(int* destination, int* source, int len);
+void start_timer();
+double end_timer();
+int get_value_one();
