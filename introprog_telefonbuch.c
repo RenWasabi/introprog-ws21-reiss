d9df85d8e12f46d6b29c5efa6f09f161bb135019
Carolina Reiß <reiss@campus.tu-berlin.de>
2021-12-20 19:55:27 +0100

    insert_node Funktion hinzugefügt, jedoch scheint funktioniert der Parent-Pointer noch nicht richtig

--- introprog_telefonbuch.c
+++ introprog_telefonbuch.c
@@ -11,7 +11,62 @@
  * gleich (<=) und alle rechten Kinder einen Wert größer (>) haben.
  */
 void bst_insert_node(bstree* bst, unsigned long phone, char *name) {
+  // create and initialize new node
+  bst_node *new_node = malloc(sizeof(bst_node));
+  new_node->left = NULL;
+  new_node->right = NULL;
+  new_node->parent = NULL;
+  new_node->phone = phone;
+  new_node->name = name;
+
+  // insert
+  // 1.case: tree doesn't exist
+  if (bst == NULL){
+    return;
+  }
+  // 2.case: tree is empty
+  // (?) pseudocode line 7: if y = nil theh root(T) <- z
+  if (bst->root == NULL){
+    bst->root = new_node;
+  }
+  // 3.case: tree is not empty => insert node at correct place
+  // initalize variables for search algorithm
+  bst_node *new_node_parent = NULL; // y in pseudocode
+  bst_node *current_node = bst->root; // x in pseudocode
+
+  /* new node will be inserted as leaf => algorithm ends when current
+   * node (new node's position) is NULL-Pointer */
+  while (current_node != NULL){
+    /* new_node_parent goes down one level (becomes current position),
+     * (?) pseudocode line 6 can be ommited because of this direct assignment */
+    new_node->parent = current_node;
+    /* current node goes down one level 
+     * (becomes left or right child after comparison of phone number value) */
+    // 1. case: number already exists in tree
+    if (new_node->phone == current_node->phone){
+      // error message to stout or sterror?
+      printf("Für diese Telefonnummer existiert bereits ein Eintrag. Abbruch...\n");
+      return;
+    } // new number smaller than current position's number => go down left
+    else if (new_node->phone < current_node->phone){
+      current_node = current_node->left;  // x <- lc(x)
+    }
+    // new number greater than current position's number => go down right
+    else {
+      current_node = current_node->right;  // x <- rc(x)
+    }
+
+    // assign new node as child of its new parents
+    if (new_node->phone < new_node->parent->phone){
+      new_node->parent->left = new_node;
+    }
+    else {
+      new_node->parent->right = new_node;
+    }  
+  }
+  return;   
 }
+
 
 /*
  * Diese Funktion liefert einen Zeiger auf einen Knoten im Baum mit
